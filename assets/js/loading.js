ad71c17b76817deb43ddeec381664b47d2c6dc82
ChiyosoXo <ardannext30@gmail.com>
2025-05-08 15:40:00 +0700

    Delete assets directory

--- assets/js/loading.js
+++
@@ -1,7 +0,0 @@
- onload + function() {
-    let id = document.getElementById("app")
-            let loading = document.createElement("div")
-
-            loading.textContent = "MoguChan"
-            id.appendChild(MoguChan)
- }
